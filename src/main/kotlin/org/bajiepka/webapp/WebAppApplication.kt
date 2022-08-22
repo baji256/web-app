740f477836ca6c75c880f4caefd0e2c3dc71704c
baji256 <bajiepka@gmail.com>
2022-08-22 16:26:53 +0300

    first-commit

---
+++ src/main/kotlin/org/bajiepka/webapp/WebAppApplication.kt
@@ -0,0 +1,11 @@
+package org.bajiepka.webapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebAppApplication>(*args)
+}
---
+++ src/test/kotlin/org/bajiepka/webapp/WebAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.bajiepka.webapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
